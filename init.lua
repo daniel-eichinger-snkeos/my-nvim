3bab7a696bf333a7bbf26e03c80781646c240519
Daniel Eichinger <daniel.eichinger@snke.com>
2026-04-17 13:03:30 +0200

    feat: Display coverage

--- init.lua
+++ init.lua
@@ -872,33 +872,33 @@ require('lazy').setup({
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-  {
-    --   -- allows pretty nice things like :GoTest and :GoLint
-    'ray-x/go.nvim',
-    dependencies = { -- optional packages
-      'ray-x/guihua.lua',
-      'neovim/nvim-lspconfig',
-      'nvim-treesitter/nvim-treesitter',
-    },
-    opts = function()
-      require('go').setup(opts)
-      local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        pattern = '*.go',
-        callback = function()
-          require('go.format').goimports()
-        end,
-        group = format_sync_grp,
-      })
-      return {
-        -- lsp_keymaps = false,
-        -- other options
-      }
-    end,
-    event = { 'CmdlineEnter' },
-    ft = { 'go', 'gomod' },
-    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
-  },
+  -- {
+  --   --   -- allows pretty nice things like :GoTest and :GoLint
+  --   'ray-x/go.nvim',
+  --   dependencies = { -- optional packages
+  --     'ray-x/guihua.lua',
+  --     'neovim/nvim-lspconfig',
+  --     'nvim-treesitter/nvim-treesitter',
+  --   },
+  --   opts = function()
+  --     require('go').setup(opts)
+  --     local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
+  --     vim.api.nvim_create_autocmd('BufWritePre', {
+  --       pattern = '*.go',
+  --       callback = function()
+  --         require('go.format').goimports()
+  --       end,
+  --       group = format_sync_grp,
+  --     })
+  --     return {
+  --       -- lsp_keymaps = false,
+  --       -- other options
+  --     }
+  --   end,
+  --   event = { 'CmdlineEnter' },
+  --   ft = { 'go', 'gomod' },
+  --   build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+  -- },
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
