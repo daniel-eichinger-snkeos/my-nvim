20112e48a04d4598e2c7d90d2bc6cc01513537bd
Daniel Eichinger <deichinger0@gmail.com>
2026-05-01 20:32:26 +0200

    feat: Add extension for agentskills

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -4,10 +4,24 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-treesitter/nvim-treesitter',
     'github/copilot.vim',
+    'cairijun/codecompanion-agentskills.nvim',
   },
   opts = {
     opts = {
       log_level = 'DEBUG',
     },
   },
+  config = function()
+    require('codecompanion').setup {
+      extensions = {
+        agentskills = {
+          opts = {
+            paths = {
+              { '~/.config/nvim/skills/.agents/skills', recursive = true }, -- Recursive search
+            },
+          },
+        },
+      },
+    }
+  end,
 }
