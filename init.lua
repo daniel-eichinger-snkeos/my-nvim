ebfd0da0ee8bf7412271cab0c153151e6a8cd7ae
Daniel Eichinger <daniel.eichinger@snke.com>
2026-05-20 16:44:18 +0200

    fix: Refine live grep

--- init.lua
+++ init.lua
@@ -382,7 +382,19 @@ require('lazy').setup({
       end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', function()
+        require('telescope.builtin').live_grep {
+          additional_args = function()
+            return {
+              '--hidden',
+              '--glob',
+              '!.git/*',
+              '--glob',
+              '!node_modules/*',
+            }
+          end,
+        }
+      end, { desc = '[S]earch by [G]rep (hidden, no .git/node_modules)' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -763,7 +775,7 @@ require('lazy').setup({
           args = {
             '-NoProfile',
             '-Command',
-            '[Console]::In.ReadToEnd() | Invoke-Formatter | Out-String | %{ ($_.TrimEnd("`r", "`n")) + "`n" }',
+            '[Console]::In.ReadToEnd() | Invoke-Formatter | Out-String | %{ ($_.TrimEnd("`r", "`n")) + "`r`n" }',
           },
           stdin = true,
         },
